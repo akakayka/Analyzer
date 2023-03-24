3f210dd242815b11675c22b6d2c6ce2c93ddf049
akakayka <akakayka@DESKTOP-T2BUV5D>
2023-03-24 22:37:35 +0300

    Добавьте файлы проекта.

---
+++ Analyzer/Analyzer.CodeFixes/AnalyzerCodeFixProvider.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Formatting;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Rename;
+using Microsoft.CodeAnalysis.Text;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Composition;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Analyzer
+{
+    [ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(AnalyzerCodeFixProvider)), Shared]
+    public class AnalyzerCodeFixProvider : CodeFixProvider
+    {
+        public sealed override ImmutableArray<string> FixableDiagnosticIds
+        {
+            get { return ImmutableArray.Create(VarAnalyzer.DiagnosticId); }
+        }
+
+        public sealed override FixAllProvider GetFixAllProvider()
+        {
+            return WellKnownFixAllProviders.BatchFixer;
+        }
+
+        public sealed override async Task RegisterCodeFixesAsync(CodeFixContext context)
+        {
+            var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
+
+            var diagnostic = context.Diagnostics.First();
+            var diagnosticSpan = diagnostic.Location.SourceSpan;
+
+            if (root.FindToken(diagnosticSpan.Start).Parent.AncestorsAndSelf().OfType<ForEachStatementSyntax>().Count() != 0)
+            {
+                var declaration = root.FindToken(diagnosticSpan.Start).Parent.AncestorsAndSelf().OfType<ForEachStatementSyntax>().First();
+                Func<ForEachStatementSyntax, TypeSyntax, ForEachStatementSyntax> WithType = (x, type) => x.WithType(type);
+
+                context.RegisterCodeFix(
+                CodeAction.Create(
+                    title: CodeFixResources.CodeFixTitle,
+                createChangedDocument: c => MakeVarAsync(context.Document, declaration, c, WithType),
+                equivalenceKey: nameof(CodeFixResources.CodeFixTitle)),
+                diagnostic);
+            }
+            else
+            {
+                Func<VariableDeclarationSyntax, TypeSyntax, VariableDeclarationSyntax> WithType = (x, type) => x.WithType(type);
+                var declaration = root.FindToken(diagnosticSpan.Start).Parent.AncestorsAndSelf().OfType<VariableDeclarationSyntax>().First();
+
+                context.RegisterCodeFix(
+                CodeAction.Create(
+                    title: CodeFixResources.CodeFixTitle,
+                createChangedDocument: c => MakeVarAsync<VariableDeclarationSyntax>(context.Document, declaration, c, WithType),
+                equivalenceKey: nameof(CodeFixResources.CodeFixTitle)),
+                diagnostic);
+            }
+
+        }
+
+        private static async Task<Document> MakeVarAsync<T>(Document document,
+    T variableDeclaration,
+    CancellationToken cancellationToken,
+    Func<T, TypeSyntax, T> WithType)
+            where T: SyntaxNode
+        {
+            SyntaxToken firstToken = variableDeclaration.GetFirstToken();
+            T trimmedLocal = variableDeclaration.ReplaceToken(
+                firstToken, firstToken.WithLeadingTrivia(SyntaxTriviaList.Empty));
+
+            TypeSyntax newType = SyntaxFactory.ParseTypeName("ауе");
+            T newLocal = WithType(trimmedLocal,newType);
+            T formattedLocal = newLocal.WithAdditionalAnnotations(Formatter.Annotation);
+
+            SyntaxNode oldRoot = await document.GetSyntaxRootAsync(cancellationToken).ConfigureAwait(false);
+            SyntaxNode newRoot = oldRoot.ReplaceNode(variableDeclaration, formattedLocal);
+
+            return document.WithSyntaxRoot(newRoot);
+        }
+        
+    }
+
+    
+}
---
+++ Analyzer/Analyzer/AnalyzerAnalyzer.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Data.Common;
+using System.IO;
+using System.Linq;
+using System.Threading;
+
+namespace Analyzer
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class VarAnalyzer : DiagnosticAnalyzer
+    {
+        public const string DiagnosticId = "Analyzer";
+
+        private static readonly LocalizableString Title = new LocalizableResourceString(nameof(Resources.AnalyzerTitle), Resources.ResourceManager, typeof(Resources));
+        private static readonly LocalizableString MessageFormat = new LocalizableResourceString(nameof(Resources.AnalyzerMessageFormat), Resources.ResourceManager, typeof(Resources));
+        private static readonly LocalizableString Description = new LocalizableResourceString(nameof(Resources.AnalyzerDescription), Resources.ResourceManager, typeof(Resources));
+        private const string Category = "Usage";
+
+        private static readonly DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning, isEnabledByDefault: true, description: Description);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get { return ImmutableArray.Create(Rule); } }
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+            context.EnableConcurrentExecution();
+            context.RegisterSyntaxNodeAction(AnalyzeNode, SyntaxKind.VariableDeclaration);
+            context.RegisterSyntaxNodeAction(AnalyzeForeachNode, SyntaxKind.ForEachStatement);
+        }
+
+        private void AnalyzeForeachNode(SyntaxNodeAnalysisContext context)
+        {
+            var foreachStatement = (ForEachStatementSyntax)context.Node;
+            
+            if (foreachStatement.Type.IsVar)
+            {
+                return;
+            }
+            context.ReportDiagnostic(Diagnostic.Create
+                (Rule, context.Node.ChildNodes().Where(x => x.GetType() == typeof(PredefinedTypeSyntax)).First().GetLocation(), foreachStatement.Identifier.ValueText));
+        }
+        private void AnalyzeNode(SyntaxNodeAnalysisContext context)
+        {
+            var variableDeclaration = (VariableDeclarationSyntax)context.Node;
+            if ((variableDeclaration.Parent.GetType() != typeof(LocalDeclarationStatementSyntax)
+                && variableDeclaration.Parent.GetType() != typeof(ForStatementSyntax)) || variableDeclaration.Type.IsVar)
+            {
+                return;
+            }
+            if (!variableDeclaration.ToString().Contains('='))
+            {
+                return;
+            }
+            context.ReportDiagnostic(Diagnostic.Create
+                (Rule, context.Node.ChildNodes().Where(x => x.GetType() == typeof(PredefinedTypeSyntax) || x.GetType() == typeof(GenericNameSyntax) || x.GetType() == typeof(IdentifierNameSyntax)).First().GetLocation(), variableDeclaration.Type.ToString()));
+        }
+    }
+}
